239390c76e1fa70c97fc9b2eb4f50a0ad54e9925
joy <joy.lai@zg.com>
2019-07-28 01:42:03 +0800

    add

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,20 @@
 import '@pefish/js-node-assist'
 import Starter from '@pefish/js-util-starter'
+import ConfigUtil from '@pefish/js-util-config'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      logger: any,
+      config: object;
+      debug: boolean;
+    }
+  }
+}
 
 Starter.startAsync(async () => {
+  global.config = ConfigUtil.loadConfig()
+  global.debug = global.config[`env`] !== 'prod'
   global.logger.info(`test`)
 }, null, true)
 
