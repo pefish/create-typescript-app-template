95d75610d51ceb1006caf90556d674a3131ea73c
pefish <pefish@qq.com>
2020-09-11 13:51:06 +0800

    fix

--- src/start.ts
+++ src/start.ts
@@ -1,7 +1,6 @@
 import '@pefish/js-node-assist'
 import Starter from '@pefish/js-util-starter'
 import ConfigUtil from '@pefish/js-util-config'
-import { Log4js, GcloudLogging } from '@pefish/js-helper-logger'
 declare global {
   namespace NodeJS {
     interface Global {
@@ -14,14 +13,10 @@ declare global {
 
 global.config = ConfigUtil.loadYamlConfig()
 global.debug = global.config.env !== 'prod'
-if (global.debug) {
-  global.logger = new Log4js()
-} else {
-  global.logger = new GcloudLogging()
-}
+global.logger = console
 
 Starter.startAsync(async () => {
   global.logger.info(global.config)
-}, null, true)
+})
 
 
